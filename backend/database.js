9011e18d05cd8c07acefdfb3c4aa3f23cddf7c75
HckR01 <139985239+HckR01@users.noreply.github.com>
2026-04-14 16:39:54 +0530

    1.CreatSearver

--- backend/database.js
+++ backend/database.js
@@ -1,2 +1,26 @@
-require(mongodb);
-const URI = "mongodb+srv://NamasteDev:Dairy@1234@namastenode.qhtmwfd.mongodb.net/?appName=NamasteNode"
+const { MongoClient } = require('mongodb');
+
+// Connection URL
+
+const url = "mongodb+srv://sarojkumarbaral6338_db_user:Dairy1234@dairyfarm.z3ugoho.mongodb.net/?appName=dairyfarm";
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'User';
+
+async function main() {
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('userInfo');
+    //read
+    const findResult = await collection.find({}).toArray();
+console.log('Found documents =>', findResult);
+
+    return 'done.';
+}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
